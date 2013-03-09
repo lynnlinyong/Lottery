189d108c5d8a84d2225ebbb191b33455fe423cd6
lynnlinyong <lynn.linyong@gmail.com>
2013-03-10 00:26:57 +0800

    Initial Commit

---
+++ Lottery/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lottery
+//
+//  Created by lynn  lynn on 13-3-10.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
